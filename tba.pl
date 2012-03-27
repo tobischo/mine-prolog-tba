ef9c4af433e68b9a34603053c86291decfd293f2
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2012-03-27 15:45:38 +0200

    language change to english

--- tba.pl
+++ tba.pl
@@ -1,23 +1,28 @@
-location(weinkeller).
-location(flurUG).
-location(fitnessraum).
-location(heizraum).
-location(sauna).
-location(entspannungsraum).
+init:-write('(c) David Hildenbrand, Tobias Schoknecht'),nl,write('Knowledge Based Systems Project'),nl,nl,write('You wake up in a dark room'),true.
 
-door(weinkeller,flurUG).
-door(fitnessraum,flurUG).
-door(entspannungsraum,flurUG).
-door(sauna,flurUG).
-door(heizraum,flurUG).
+printlocation:-position(X),write('You are in the '),write(X).
+
+location('wine cellar').
+location('corridorUG').
+location('fitnessroom').
+location('boiler room').
+location('sauna').
+location('relaxation room').
+
+door('wine cellar','corridorUG').
+door('fitnessroom','corridorUG').
+door('relaxation room','corridorUG').
+door('sauna','corridorUG').
+door('boiler room','corridorUG').
 %door(X,Y):-door(Y,X).
 
-movementRule(heizraum,flurUG):-inventory([keyA]).
+movementRule('relaxation room','corridorUG'):-inventory([keyA]).
 
-stairs(flurUG,flurEG).
-stairs(flurEG,flurUG).
+stairs('corridorUG','corridorEG').
+stairs('corridorEG','corridorUG').
 
 go_to(X):-position(Y),movementRule(Y,X)->retract(position(Y)),asserta(position(X));write('failed'),false.
 
 %initialize Game
-startGame:-assert(position(heizraum)),assert(inventory([])).
+startGame:-init,assert(position('boiler room')),assert(inventory([])).
+
